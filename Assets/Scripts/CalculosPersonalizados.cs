f9fe32703b58c660d876c03b4d8a2a5cbe9073c4
FranWeinmann <weinmann.fran@gmail.com>
2025-03-28 10:41:05 -0300

    Hice hasta el 4

---
+++ Assets/Scripts/CalculosPersonalizados.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculosPersonalizados : MonoBehaviour
+{
+
+    public enum options
+    {
+        Suma,
+        Resta,
+        Dividir,
+        Multiplicar
+    }
+
+    public options opcion;
+    public float num1;
+    public float num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float resultadoSuma = num1 + num2;
+        float resultadoResta = num1 - num2;
+        float resultadoMultiplicacion = num1 * num2;
+        float resultadoDivision = num1 / num2;
+        Debug.Log("El resultado de la suma entre " + num1 + " y " + num2 + ", es: " + resultadoSuma + ". " + 
+            "El resultado de la resta entre " + num1 + " y " + num2 + ", es: " + resultadoResta + ". " +
+            "El resultado de la multiplicación entre " + num1 + " y " + num2 + ", es: " + resultadoMultiplicacion + ". " +
+            "El resultado de la división entre " + num1 + " y " + num2 + ", es: " + resultadoDivision + ". ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+
+    public string userName;
+    public int userAge;
+    public float userHeigth;
+    public bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (esDonante)
+        {
+            Debug.Log("Mi nombre es " + userName + ", tengo " + userAge + ", mido " + userHeigth + "m y es verdad que soy donante de órganos");
+        }
+        else
+        {
+            Debug.Log("Mi nombre es " + userName + ", tengo " + userAge + ", mido " + userHeigth + "m y es mentira que soy donante de órganos");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
